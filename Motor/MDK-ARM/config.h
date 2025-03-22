b3dfb233278bfe74a2deaed78f9b646d73183205
zhuxu-dell g3 <1018128765@qq.com>
2025-03-22 20:02:45 +0800

    SV0.0.1 首次上传 添加编码器读取功能

---
+++ Motor/MDK-ARM/config.h
@@ -0,0 +1,6 @@
+#ifndef  __CONFIG_H__
+#define	 __CONFIG_H__
+
+#define MotorSpeedCalFreq  1000
+
+#endif
---
+++ Motor/MDK-ARM/define.h
@@ -0,0 +1,13 @@
+#ifndef __DEFINE_H__
+#define __DEFINE_H__
+
+#include "config.h"
+#include "main.h"
+#include "dma.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+
+#endif
